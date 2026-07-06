3a2705119e9a7a2c670d6ba0c21287446e4c062a
funnythanh-glitch <funnythanh@gmail.com>
2026-07-06 23:56:22 +0700

    Thanh commit lan 5

---
+++ Bai 4.2 - 06.07.2026/Bai 4.2 - 06.07.2026/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace Bai_4._2___06._07._2026
+{
+    internal class Program
+    {
+        #region Hàm không trả về giá trị, không có tham số
+        static void Display()
+        {
+            int x = 0;
+            int y = 10;
+            int z = x + y;
+            Console.WriteLine($"giá trị z: {z}");
+
+        }
+        #endregion
+
+        #region Hàm không trả về giá trị, có truyền tham số
+        static void DisplayV2(int x ,int y)
+        {
+            int z = x + y;
+            Console.WriteLine($"giá trị z (V2): {z}");
+
+        }
+        #endregion
+
+        #region Hàm có trả về giá trị, không tham số
+        static float TinhDienTich()
+        {
+            float chieuDai = 4.5F;
+            float chieuRong = 2.5F;
+            float dienTich = chieuDai * chieuRong;
+            return dienTich;
+        }
+        #endregion
+
+        #region Hàm có trả về giá trị, có tham số
+        static float TinhDienTichV2(float chieuDai, float chieuRong)
+        {
+            float dienTich = chieuDai * chieuRong;
+            return dienTich;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            Display();
+            float dienTich = TinhDienTich();
+            Console.WriteLine("Diện Tích : " +dienTich);
+
+            int x = 0;
+            double y = 10;  
+            DisplayV2(x, (int)y);
+
+            float chieuDai = 4.5F;
+            float chieuRong = 2.5F;
+            float dienTichV2 = TinhDienTichV2(chieuDai, chieuRong);
+            Console.WriteLine("Diện Tích V2 : " + dienTichV2);
+        }
+    }
+}
---
+++ Buoi 4 - 06.07.2026/Buoi 4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Buoi_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            //Toán Tử Logic
+            if ( 5 == 5 && 6 == 7) 
+            {
+                Console.WriteLine("Đúng");
+            }
+            else
+            {
+                Console.WriteLine("Sai");
+            }
+
+            // Toán tử 3 ngôi
+            int x = 5;
+            int y = 6;
+            String result = x == y ? "x bằng y" : "x khác y";
+            Console.WriteLine("giá trị result: " + result);
+
+            // Toán tử gán
+            int a = 10;
+            a += 5;
+            Console.WriteLine("Kết Quả = " + a);
+
+            // Toán tử tăng giảm
+            // a++ : sử dụng giá trị trước sau mới tăng
+            // ++a : tăng giá trị trước rồi mới đi sử dụng
+
+            int b = 5;
+            Console.WriteLine($"Giá Trị b: {b++}");
+            Console.WriteLine($"Giá Trị b++: {b}");
+
+            Console.WriteLine($"Giá Trị ++b: {++b}");
+            Console.WriteLine($"Giá Trị b: {b}");
+
+            //Toán từ NUll
+            string name = null;
+            string nameV2 = "";
+            string nameV3 = " "; // độ dùa ký tự bằng 1, chuỗi
+
+            int? age = null; // nullbale value type
+            // Nullbale<int> age = null;
+            int defaultAge = age ?? 18;
+            int total = age ?? 18; // nếu mà biến age là null thì tôi lấy mặc định giá trị là 18
+            Console.WriteLine("giá trị total: " + total);
+        }
+    }
+}
