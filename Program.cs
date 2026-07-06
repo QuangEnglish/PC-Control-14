614f3ee0ced412f5369c24cfcbb38d2440fbc297
binh172 <Ntbinh172@gmail.com>
2026-07-06 21:02:02 +0700

    btvn 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 5.77F;
+            Console.WriteLine(Convert.ToInt32(a));
+            Console.WriteLine((int)a);
+            int b = 50;
+            long c = b;
+            Console.WriteLine(c);
+        }
+    }
+}
