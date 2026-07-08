8330cbc1fb3df55251d0e6587189493f1e76fcd0
Haihhbg98 <betapchoi1998vn@gmail.com>
2026-07-08 16:23:02 +0700

    Bai_tap3_TinhTienDien_Ham_KieuTraVeCoVaKhongCoThamSo

---
+++ Bai_tap3_TinhTienDien_Ham_KieuTraVeCoVaKhongCoThamSo/Bai_tap3_Ham_KieuTraVeCoVaKhongCoThamSo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+namespace Bai_tap3_Ham_KieuTraVeCoVaKhongCoThamSo
+{
+    internal class Program
+    {
+        #region Ham tinh so tien dien 
+        static decimal TinhSoTienDien(int a)
+        {
+            decimal soTien = 0;
+            if (a <= 50)
+            {
+                soTien = a * 1800;
+            }
+            else if(a > 50 & a <= 100)
+            {
+                soTien = a * 2000;
+            }
+            else
+            {
+                soTien = a * 2500;
+            }
+                return soTien;
+            
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Nhập số điện :");
+            int soDien = int.Parse (Console.ReadLine());
+
+            decimal soTienDien = TinhSoTienDien(soDien);
+            Console.WriteLine($"Số Tiền Điện Tháng Này : {soTienDien} VND" );
+        }
+    }
+}
