311d38f3ab22bd0ec11b23dbded4b74bad781284
QuangEnglish <hquanganh123@gmail.com>
2026-07-25 00:45:51 +0700

    Lesson 11 ExceptionHandling và OOP cơ bản

---
+++ Code/Lesson10-FileIO-ExceptionHandling/Lesson10-FileIO-ExceptionHandling/DeviceConnectException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class DeviceConnectException : Exception
+    {
+        public string PortName { get; }  // tên cổng cơm, cổng kết nối
+        public int BaudRate { get; } // tốc độ truyền
+        public int RetryCount { get; } // số lần kết nối lại
+
+        public DeviceConnectException() : base("Kết nối thiết bị thất bại.") { }
+
+        public DeviceConnectException(string message) : base(message) { }
+
+        // Constructor 3: message + inner exception (rất quan trọng!)
+        public DeviceConnectException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        // Constructor 4: đầy đủ – dùng trong PC Control
+        public DeviceConnectException(string portName, int baudRate, int retryCount, string message, Exception inner = null)
+            : base(message, inner)
+        {
+            PortName = portName;
+            BaudRate = baudRate;
+            RetryCount = retryCount;
+
+            // Lưu thêm vào Data dictionary (tùy chọn)
+            Data["Port"] = portName;
+            Data["Baud"] = baudRate;
+        }
+
+        // Serialization constructor – hỗ trợ remoting / AppDomain (best practice)
+        protected DeviceConnectException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            PortName = info.GetString(nameof(PortName));
+            BaudRate = info.GetInt32(nameof(BaudRate));
+            RetryCount = info.GetInt32(nameof(RetryCount));
+        }
+
+        // Override GetObjectData nếu có serialization
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue(nameof(PortName), PortName);
+            info.AddValue(nameof(BaudRate), BaudRate);
+            info.AddValue(nameof(RetryCount), RetryCount);
+        }
+    }
+}
---
+++ Code/Lesson11-OOP-Start/Lesson11-OOP-Start/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson11_OOP_Start
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // OOP là 1 cách để tổ chức code xung quan đối tượng 
+            // dễ quản lý code
+
+            // Đối tượng- Object
+            // Thuộc tính 
+            // Hành Vi 
+
+            // Class và Object
+            // Class (Lớp) là 1 bản thiết kế hoặc khuôn mẫu, gồm thuộc tính và hành vi
+            // Object (Đối tượng) là 1 thực thể cụ thể được sản xuất từ cái bản vẽ Class 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            XeHoi xeHoiCuaAn = new XeHoi();
+            xeHoiCuaAn.TocDo = 100000000;
+            xeHoiCuaAn.MauSac = "Xanh";
+            xeHoiCuaAn.NamSanXuat = 2000;
+            xeHoiCuaAn.Chay();
+
+            XeHoi xeHoiCuaAnCap3 = new XeHoi(20, 30);
+            xeHoiCuaAnCap3.MauSac = "Xanh";
+            xeHoiCuaAnCap3.Chay();
+
+            // 4 tính chất OOP
+
+
+        }
+    }
+}
